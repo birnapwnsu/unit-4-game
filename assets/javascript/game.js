8ca47ee41e651367e8b0ac2ca5e541d423a2b46f
birna <briandam26@yahoo.com>
2019-05-28 17:02:36 -0700

    structured the page on html, started some styles, and added more to the game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
-//array of characters
+$(document).ready(function() {
 
+//array of characters
 const characters = {
     'yoda': {
         name: 'yoda',
@@ -35,7 +36,6 @@ const characters = {
 };
 
 //variables
-
 let currSelected;
 let currDefender;
 let combatants = [];
@@ -45,7 +45,6 @@ let turnCounter = 1;
 let killCount = 0;
 
 //make character function which results in obj, class/id, and string
-
 let renderOne = function(character, renderArea, makeChar) {
     let charDiv = $("<div class='character' data-name='" + character.name + "'>");
     let charName = $("<div class='character-name'>").text(character.name);
@@ -53,18 +52,102 @@ let renderOne = function(character, renderArea, makeChar) {
     let charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
     charDiv.append(charName).append(charHealth);
     $(renderArea).append(charDiv);
+    if (makeChar == 'enemy') {
+        $(charDiv).addClass('enemy');
+    }else if (makeChar == 'defender') {
+        currDefender = character;
+        $(charDiv).addClass('target-enemy');
+    }
 }
 
+// Create function to render game message to DOM
+let renderMessage = function(message) {
+    let gameMesageSet = $("#gameMessage");
+    let newMessage = $("<div>").text(message);
+    gameMesageSet.append(newMessage);
+
+    if (message == 'clearMessage') {
+      gameMesageSet.text('');
+    }
+};
+
 //renders all characters
+if (areaRender == '#characters-section') {
+    $(areaRender).empty();
+    for (var key in charObj) {
+        if (charObj.hasOwnProperty(key)) {
+            renderOne(charObj[key], areaRender, '');
+        }
+    }
+}
 
 //render player 
+if (areaRender == '#selected') {
+    $('#selected').prepend("Your Character");       
+    renderOne(charObj, areaRender, '');
+    $('#attack-button').css('visibility', 'visible');
+  }
 
 //render combatants
+if (areaRender == '#targets-section') {
+    $('#targets-section').prepend("Choose Your Next Opponent");      
+  for (var i = 0; i < charObj.length; i++) {
+
+    renderOne(charObj[i], areaRender, 'enemy');
+  }
 
 //render one enemy as defender
+$(document).on('click', '.enemy', function() {
+    //select an combatant to fight
+    name = ($(this).data('name'));
+    //if defernder area is empty
+    if ($('#defender').children().length === 0) {
+        renderCharacters(name, '#defender');
+        $(this).hide();
+        renderMessage("clearMessage");
+        }
+    });
+}
+//render defender
+if (areaRender == '#defender') {
+    $(areaRender).empty();
+    for (var i = 0; i < combatants.length; i++) {
+    //add enemy to defender area
+        if (combatants[i].name == charObj) {
+        $('#defender').append("Your selected opponent")
+        renderOne(combatants[i], areaRender, 'defender');
+        }
+    }
+}
 
 //re-render defender when attacked
+if (areaRender == 'playerDamage') {
+    $('#defender').empty();
+    $('#defender').append("Your selected opponent")
+    renderOne(charObj, '#defender', 'defender');
+}
 
 //re-render player when attacked
+if (areaRender == 'enemyDamage') {
+    $('#selected').empty();
+    renderOne(charObj, '#selected', '');
+}
 
 //render defeated enemy
+if (areaRender == 'enemyDefeated') {
+    $('#defender').empty();
+    var gameStateMessage = "You have defated " + charObj.name + ", you can choose to fight another enemy.";
+    renderMessage(gameStateMessage);
+    }
+
+//need to render characters for users to choose their character
+//need to render characters for users to choose to fight against
+//need functions to allow actions between objects
+// -action done to defender reflected
+// -damage shown as result of combat
+// -win condition otherwise game continues
+
+//need a function restart the game
+
+});
+
